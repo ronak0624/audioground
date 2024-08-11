e8e60a04ec981d443535cd4b5960ae2f12accf5b
ronak0624 <ronak0624@gmail.com>
2024-08-10 18:28:29 -0700

    model manifest

--- python/labeler.py
+++ python/labeler.py
@@ -1,4 +1,24 @@
 # Class to take list of paths as input and return list of AudioFeatures objects
+import json
+import os
+
+
+class ModelManifest:
+    def __init__(self, model):
+        self.model = model
+        self.model_path = os.path.join(
+            os.path.dirname(__file__), "models", f"{model}.json"
+        )
+        with open(self.model_path, "r") as f:
+            self.model_manifest = json.load(f)
+
+    def get_classes(self):
+        return self.model_manifest["classes"]
+
+    def get_algorithm(self):
+        return self.model_manifest["inference"]["algorithm"]
+
+
 class AudioFeatures:
     def __init__(self, paths):
         self.paths = paths
--- python/labeler_test.py
+++ python/labeler_test.py
@@ -1,4 +1,4 @@
-from labeler import AudioFeatures
+from labeler import AudioFeatures, ModelManifest
 import unittest
 
 
@@ -7,3 +7,10 @@ class TestAudioFeatures(unittest.TestCase):
         paths = ["path1", "path2"]
         audio_features = AudioFeatures(paths)
         self.assertEqual(audio_features.get_paths(), paths)
+
+    def test_model_manifest(self):
+        model_manifest = ModelManifest("discogs-effnet-bs64-1")
+        self.assertTrue(isinstance(model_manifest.get_classes(), list))
+        self.assertEqual(
+            model_manifest.get_algorithm(), "TensorflowPredictEffnetDiscogs"
+        )
