61d81205ac32e9453c6fbf924ed81e80844114c5
ronak0624 <ronak0624@gmail.com>
2024-08-12 15:59:00 -0700

    macos titlebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Interface from "./Interface";
 import { Toaster } from "./components/ui/sonner";
 import { toast } from "sonner";
 import { useEffect } from "react";
+import Titlebar from "./components/Titlebar";
 
 function App() {
   const [done, loading, error] = usePythonVenv();
@@ -21,6 +22,7 @@ function App() {
 
   return (
     <>
+      <Titlebar />
       <Interface />
       <Toaster richColors />
     </>
