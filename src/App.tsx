de189985698ca86efdb3240ec7f799cac5e48804
ronak0624 <ronak0624@gmail.com>
2024-08-12 00:52:59 -0700

    fullscreen container, toastbar for alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import usePythonVenv from "./lib/shell/python";
 import Interface from "./Interface";
+import { Toaster } from "./components/ui/sonner";
+import { toast } from "sonner";
+import { useEffect } from "react";
 
 function App() {
   const [done, loading, error] = usePythonVenv();
 
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+    if (done) {
+      toast.success("Dependencies installed!");
+    }
+    if (loading) {
+      toast.info("Installing dependencies...");
+    }
+  }, [done, loading, error]);
+
   return (
     <>
-      {loading && <p>Loading dependencies...</p>}
-      {error && <p>Error: {error}</p>}
-      {done && <p>Dependencies installed!</p>}
       <Interface />
+      <Toaster />
     </>
   );
 }
